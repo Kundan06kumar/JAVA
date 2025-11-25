7facbcad9959a4874f0c84971f82c927f5675138
Kundan06kumar <kundan0618.in@gmail.com>
2025-11-25 11:34:17 +0530

    Create swap_two_no2.java

---
+++ swap_two_no2.java
@@ -0,0 +1,10 @@
+//swap two no. without using third variable
+public class SwapNoTemp {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}

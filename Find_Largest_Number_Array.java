a87730d2f29baf537686df6f8f07a319aee34542
Kundan06kumar <kundan0618.in@gmail.com>
2025-11-28 16:17:40 +0530

    Create Find_Largest_Number_Array.java

---
+++ Find_Largest_Number_Array.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {5, 12, 10, 20, 8};
+
+        int max = arr[0];
+
+        for(int i=1; i<arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Largest = " + max);
+    }
+}
+//array mein largest element

4cf57c2bccb236eb7ad9c05f0aec5752972efc6d
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-05 18:55:36 +0530

    Create butterfly_pattern.java

---
+++ butterfly_pattern.java
@@ -0,0 +1,25 @@
+// *      *
+// **    **
+// ***  ***
+// ********
+// ********
+// ***  ***
+// **    **
+// *      *
+int n = 4;
+
+// Upper part
+for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) System.out.print("*");
+    for (int s = 1; s <= 2*(n-i); s++) System.out.print(" ");
+    for (int j = 1; j <= i; j++) System.out.print("*");
+    System.out.println();
+}
+
+// Lower part
+for (int i = n; i >= 1; i--) {
+    for (int j = 1; j <= i; j++) System.out.print("*");
+    for (int s = 1; s <= 2*(n-i); s++) System.out.print(" ");
+    for (int j = 1; j <= i; j++) System.out.print("*");
+    System.out.println();
+}

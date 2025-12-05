dae3670198b2bd28e2a6792200d1192ef3328d33
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-05 18:52:10 +0530

    Create diamond_pattern.java

---
+++ diamond_pattern.java
@@ -0,0 +1,35 @@
+//    *
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+public class DiamondPattern {
+    public static void main(String[] args) {
+        int n = 4; // height of upper pyramid
+
+        // Upper part
+        for(int i = 1; i <= n; i++){
+            for(int s = 1; s <= n - i; s++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= 2*i - 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower part
+        for(int i = n-1; i >= 1; i--){
+            for(int s = 1; s <= n - i; s++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= 2*i - 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+

bbd8713dcea0de1986e2f85ec2a2b948479c8108
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-07 18:26:33 +0530

    Create hollow_diamond.java

---
+++ hollow_diamond.java
@@ -0,0 +1,36 @@
+ //    *
+ //   * *
+ //  *   *
+ // *     *
+ //  *   *
+ //   * *
+ //    *
+int n = 5;
+
+// upper hollow pyramid
+for(int i=1;i<=n;i++){
+    for(int s=1;s<=n-i;s++)
+        System.out.print(" ");
+
+    for(int j=1;j<=2*i-1;j++){
+        if(j==1 || j==2*i-1)
+            System.out.print("*");
+        else
+            System.out.print(" ");
+    }
+    System.out.println();
+}
+
+// lower hollow pyramid
+for(int i=n-1;i>=1;i--){
+    for(int s=1;s<=n-i;s++)
+        System.out.print(" ");
+
+    for(int j=1;j<=2*i-1;j++){
+        if(j==1 || j==2*i-1)
+            System.out.print("*");
+        else
+            System.out.print(" ");
+    }
+    System.out.println();
+}

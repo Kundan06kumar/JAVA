53a9a999ab1691f3a78eba5f24f1e2bb00fd0a21
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-07 18:27:41 +0530

    Create pascal_triangle.java

---
+++ pascal_triangle.java
@@ -0,0 +1,15 @@
+// 1
+// 1 1
+// 1 2 1
+// 1 3 3 1
+int n = 4;
+
+for(int i=0;i<n;i++){
+    int num = 1;
+    for(int j=0;j<=i;j++){
+        System.out.print(num + " ");
+        num = num * (i - j) / (j + 1);
+    }
+    System.out.println();
+}
+

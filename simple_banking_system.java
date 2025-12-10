73f7a874ea3226273e1f2ee6c7e98fca56babe8c
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-10 21:27:24 +0530

    Create simple_banking_system.java

---
+++ simple_banking_system.java
@@ -0,0 +1,23 @@
+class Bank {
+    int balance = 1000;
+
+    void deposit(int amount) {
+        balance += amount;
+        System.out.println("Balance: " + balance);
+    }
+
+    void withdraw(int amount) {
+        if (amount > balance)
+            System.out.println("Insufficient Balance");
+        else {
+            balance -= amount;
+            System.out.println("Balance: " + balance);
+        }
+    }
+
+    public static void main(String[] args) {
+        Bank b = new Bank();
+        b.deposit(500);
+        b.withdraw(300);
+    }
+}

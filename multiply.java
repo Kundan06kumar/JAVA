3c28d66e986f4713a658063477a6e53400533d18
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-12 22:44:13 +0530

    Create multiply.java

---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 7;
+        int product = a * b;
+
+        System.out.println("Product = " + product);
+    }
+}
+//multiply two numbers

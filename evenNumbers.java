f4941be24fb06598aa58bfb7eb031d77970e24aa
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-13 19:38:11 +0530

    Create evenNumbers.java

---
+++ evenNumbers.java
@@ -0,0 +1,10 @@
+//Check Even Numbers from 1 to 10
+public class EvenNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}

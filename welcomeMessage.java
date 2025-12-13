10e554f472c3323ff9a7172d7e637f2a59264440
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-13 19:39:23 +0530

    Create welcomeMessage.java

---
+++ welcomeMessage.java
@@ -0,0 +1,8 @@
+//Print Welcome Message Multiple Times
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Welcome to Java Programming");
+        }
+    }
+}

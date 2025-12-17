dfc09916e434e8debc13995faf72bace24770e14
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-17 23:14:52 +0530

    Create palindrome_number.java

---
+++ palindrome_number.java
@@ -0,0 +1,15 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        int num = 121, temp = num, rev = 0;
+
+        while (num != 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+
+        if (temp == rev)
+            System.out.println("Palindrome Number");
+        else
+            System.out.println("Not Palindrome");
+    }
+}

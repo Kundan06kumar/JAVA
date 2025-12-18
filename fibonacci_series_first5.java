2d1c74c972de864d799dcc40664430c0cf7479f0
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-18 19:35:09 +0530

    Create fibonacci_series_first5.java

---
+++ fibonacci_series_first5.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int a = 0, b = 1;
+
+        System.out.print(a + " " + b + " ");
+
+        for (int i = 3; i <= 5; i++) {
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+}

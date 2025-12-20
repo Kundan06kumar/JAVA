350da765ef34e1b192980b394e11d9b023184895
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-20 22:17:46 +0530

    Create check_string_is_empty_or_not.java

---
+++ check_string_is_empty_or_not.java
@@ -0,0 +1,10 @@
+public class EmptyStringCheck {
+    public static void main(String[] args) {
+        String str = "";
+
+        if (str.isEmpty())
+            System.out.println("String is Empty");
+        else
+            System.out.println("String is Not Empty");
+    }
+}

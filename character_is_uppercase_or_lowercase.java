c90fad6dbea374d0ce4813b855b0d8ddcfdbbbfb
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-20 22:19:01 +0530

    Create character_is_uppercase_or_lowercase.java

---
+++ character_is_uppercase_or_lowercase.java
@@ -0,0 +1,10 @@
+public class CaseCheck {
+    public static void main(String[] args) {
+        char ch = 'G';
+
+        if (ch >= 'A' && ch <= 'Z')
+            System.out.println("Uppercase");
+        else
+            System.out.println("Lowercase");
+    }
+}

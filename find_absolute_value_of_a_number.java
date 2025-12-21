89e1d2bc012b7f4d5e0c8e03d8f63f9be8e00b42
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-21 23:52:54 +0530

    Create find_absolute_value_of_a_number.java

---
+++ find_absolute_value_of_a_number.java
@@ -0,0 +1,10 @@
+public class AbsoluteValue {
+    public static void main(String[] args) {
+        int num = -25;
+
+        if (num < 0)
+            num = -num;
+
+        System.out.println("Absolute Value = " + num);
+    }
+}

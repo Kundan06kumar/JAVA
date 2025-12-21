0d2989e34f9653e8beaf942507195eeae9bef653
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-21 23:53:59 +0530

    Create check_number_is_multiple_of_3_or_not.java

---
+++ check_number_is_multiple_of_3_or_not.java
@@ -0,0 +1,10 @@
+public class MultipleOfThree {
+    public static void main(String[] args) {
+        int num = 14;
+
+        if (num % 3 == 0)
+            System.out.println("Multiple of 3");
+        else
+            System.out.println("Not a Multiple of 3");
+    }
+}

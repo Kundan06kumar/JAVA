2068985d301f2bfb9d4188d9e709179f82486901
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-22 22:48:09 +0530

    Create check_no_is_sinlge_digit_or_not.java

---
+++ check_no_is_sinlge_digit_or_not.java
@@ -0,0 +1,10 @@
+public class SingleDigit {
+    public static void main(String[] args) {
+        int num = 7;
+
+        if (num >= -9 && num <= 9)
+            System.out.println("Single Digit Number");
+        else
+            System.out.println("Not a Single Digit Number");
+    }
+}

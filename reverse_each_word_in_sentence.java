a5deb66fa8612aa7f73328e987fb40698047da8f
Kundan06kumar <kundan0618.in@gmail.com>
2025-12-24 23:49:56 +0530

    Create reverse_each_word_in_sentence.java

---
+++ reverse_each_word_in_sentence.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReverseEachWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+
+        String[] words = sentence.split(" ");
+        String result = "";
+
+        for (String word : words) {
+            String rev = "";
+            for (int i = word.length() - 1; i >= 0; i--) {
+                rev += word.charAt(i);
+            }
+            result += rev + " ";
+        }
+
+        System.out.println("Result: " + result.trim());
+    }
+}

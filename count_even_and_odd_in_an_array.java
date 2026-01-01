3de52d7120aa4c3307a01949db694064c2a4bd03
Kundan06kumar <kundan0618.in@gmail.com>
2026-01-01 23:47:47 +0530

    Create count_even_and_odd_in_an_array.java

---
+++ count_even_and_odd_in_an_array.java
@@ -0,0 +1,15 @@
+public class CountEvenOdd {
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 3, 12, 7, 6};
+
+        int even = 0, odd = 0;
+
+        for(int num : arr){
+            if(num % 2 == 0) even++;
+            else odd++;
+        }
+
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
+    }
+}

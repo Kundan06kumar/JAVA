070fdfda9fe89597c7b1c338630c7d49fd7b9338
Kundan06kumar <kundan0618.in@gmail.com>
2026-01-02 14:44:43 +0530

    Create HrshadNumberCheck.java

---
+++ HrshadNumberCheck.java
@@ -0,0 +1,15 @@
+public class HarshadNumber {
+    public static void main(String[] args) {
+        int n = 18, sum = 0, temp = n;
+
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        if (n % sum == 0)
+            System.out.println("Harshad Number");
+        else
+            System.out.println("Not Harshad Number");
+    }
+}

37c0102d47cc804250e47427e56811f5497fc907
Kundan06kumar <kundan0618.in@gmail.com>
2026-01-12 23:52:11 +0530

    Create find_frequency_of_each_digit.java

---
+++ find_frequency_of_each_digit.java
@@ -0,0 +1,15 @@
+public class DigitFrequency {
+    public static void main(String[] args) {
+        int n = 1213421;
+        int[] freq = new int[10];
+
+        while(n != 0) {
+            freq[n % 10]++;
+            n /= 10;
+        }
+
+        for(int i = 0; i < 10; i++)
+            if(freq[i] > 0)
+                System.out.println(i + " -> " + freq[i]);
+    }
+}

aec531be176cd56c1108cf7eb6c5469bce1a8c41
Kundan06kumar <kundan0618.in@gmail.com>
2026-01-12 23:52:55 +0530

    Create reverese_number_using_recursion.java

---
+++ reverese_number_using_recursion.java
@@ -0,0 +1,14 @@
+public class ReverseRecursion {
+    static int rev = 0;
+
+    static void reverse(int n) {
+        if(n == 0) return;
+        rev = rev * 10 + n % 10;
+        reverse(n / 10);
+    }
+
+    public static void main(String[] args) {
+        reverse(1234);
+        System.out.println(rev);
+    }
+}

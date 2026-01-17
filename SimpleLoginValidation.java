30af04aea5d2be18d1c7bbb0cd51a28ee328103f
Kundan06kumar <kundan0618.in@gmail.com>
2026-01-17 23:53:59 +0530

    Create SimpleLoginValidation.java

---
+++ SimpleLoginValidation.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LoginValidation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String username = "admin";
+        String password = "1234";
+
+        System.out.print("Enter Username: ");
+        String u = sc.nextLine();
+
+        System.out.print("Enter Password: ");
+        String p = sc.nextLine();
+
+        if (u.equals(username) && p.equals(password)) {
+            System.out.println("Login Successful");
+        } else {
+            System.out.println("Invalid Credentials");
+        }
+
+        sc.close();
+    }
+}

2c44163b4a589d8d7e55bdffd2dc1d112b737375
Kundan06kumar <kundan0618.in@gmail.com>
2026-01-18 23:29:27 +0530

    Create count_frequency)of_each_digit_in_number.java

---
+++ count_frequency)of_each_digit_in_number.java
@@ -0,0 +1,16 @@
+class DigitFrequency {
+    public static void main(String[] args) {
+        int num = 122333;
+        int[] freq = new int[10];
+
+        while (num > 0) {
+            freq[num % 10]++;
+            num /= 10;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (freq[i] > 0)
+                System.out.println(i + " -> " + freq[i]);
+        }
+    }
+}

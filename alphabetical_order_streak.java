2349ce53380ed38dc4bf6529a92dc79d93a07bef
Kundan06kumar <kundan0618.in@gmail.com>
2026-01-26 23:47:19 +0530

    Create alphabetical_order_streak.java

---
+++ alphabetical_order_streak.java
@@ -0,0 +1,16 @@
+public class AlphabetStreak {
+    public static void main(String[] args) {
+
+        String s = "abcdfghabxyz";
+        int count = 1, max = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1) + 1) {
+                count++;
+                max = Math.max(max, count);
+            } else count = 1;
+        }
+
+        System.out.println("Longest Alphabetical Streak: " + max);
+    }
+}

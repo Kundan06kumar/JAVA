cae758e508d58724890740446dcbc6b646ab1e86
Kundan06kumar <kundan0618.in@gmail.com>
2026-02-08 22:32:18 +0530

    Create consecutive_same_character_streak.java

---
+++ consecutive_same_character_streak.java
@@ -0,0 +1,13 @@
+public class SameCharStreak {
+    public static void main(String[] args) {
+        String s = "aaabbbcccccdd";
+        int count = 1, max = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) count++;
+            else count = 1;
+            max = Math.max(max, count);
+        }
+        System.out.println(max);
+    }
+}

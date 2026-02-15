79cd7155e41a7b42db32b95d97bab3869015f008
Kundan06kumar <kundan0618.in@gmail.com>
2026-02-15 22:19:30 +0530

    Create increasing_ASCII_by_2_streak.java

---
+++ increasing_ASCII_by_2_streak.java
@@ -0,0 +1,15 @@
+public class ASCIIByTwoStreak {
+    public static void main(String[] args) {
+        String s = "acegikbdf";
+        int count = 1, max = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1) + 2)
+                count++;
+            else count = 1;
+
+            max = Math.max(max, count);
+        }
+        System.out.println(max);
+    }
+}

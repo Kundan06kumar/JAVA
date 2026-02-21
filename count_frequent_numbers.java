1c3cf7ee6a47fe885ae25f5ca0a7fcc2749a9da8
Kundan06kumar <kundan0618.in@gmail.com>
2026-02-21 23:54:26 +0530

    Create count_frequent_numbers.java

---
+++ count_frequent_numbers.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,1,3,2,1};
+
+        int maxCount = 0;
+        int mostFrequent = arr[0];
+
+        for(int i = 0; i < arr.length; i++) {
+
+            int count = 0;
+
+            for(int j = 0; j < arr.length; j++) {
+
+                if(arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+
+            if(count > maxCount) {
+                maxCount = count;
+                mostFrequent = arr[i];
+            }
+        }
+
+        System.out.println("Most frequent number: " + mostFrequent);
+        System.out.println("Frequency: " + maxCount);
+    }
+}

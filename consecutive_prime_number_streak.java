4d577bddce1eeae9695c726765098e6c74f1b344
Kundan06kumar <kundan0618.in@gmail.com>
2026-03-01 23:31:42 +0530

    Create consecutive_prime_number_streak.java

---
+++ consecutive_prime_number_streak.java
@@ -0,0 +1,22 @@
+public class ConsecutivePrimeStreak {
+
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i * i <= n; i++)
+            if (n % i == 0) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 7, 4, 11, 13};
+        int count = 0, max = 0;
+
+        for (int n : arr) {
+            if (isPrime(n)) {
+                count++;
+                max = Math.max(max, count);
+            } else count = 0;
+        }
+        System.out.println(max);
+    }
+}

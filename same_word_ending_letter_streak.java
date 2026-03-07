6773d88b0570455dd0fc8fd62fef6a1937dd6d7c
Kundan06kumar <kundan0618.in@gmail.com>
2026-03-07 23:59:28 +0530

    Create same_word_ending_letter_streak.java

---
+++ same_word_ending_letter_streak.java
@@ -0,0 +1,16 @@
+public class EndingLetterStreak {
+    public static void main(String[] args) {
+        String[] words = {"cat", "bat", "rat", "dog", "fog"};
+        int count = 1, max = 1;
+
+        for (int i = 1; i < words.length; i++) {
+            if (words[i].charAt(words[i].length() - 1) ==
+                words[i - 1].charAt(words[i - 1].length() - 1))
+                count++;
+            else count = 1;
+
+            max = Math.max(max, count);
+        }
+        System.out.println(max);
+    }
+}

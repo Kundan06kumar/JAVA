fcf971acb2770da3b193bec0449dc6f1c8d5b97e
Kundan06kumar <kundan0618.in@gmail.com>
2026-03-10 21:01:58 +0530

    Create increasing_cube_streak.java

---
+++ increasing_cube_streak.java
@@ -0,0 +1,15 @@
+public class IncreasingCubeStreak {
+    public static void main(String[] args) {
+        int[] arr = {1, 8, 27, 64, 5, 125};
+        int count = 1, max = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (Math.cbrt(arr[i]) > Math.cbrt(arr[i - 1]))
+                count++;
+            else count = 1;
+
+            max = Math.max(max, count);
+        }
+        System.out.println(max);
+    }
+}

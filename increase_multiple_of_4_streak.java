b736a3b833569d5aac4a36d4a17bf63fd9ca223c
Kundan06kumar <kundan0618.in@gmail.com>
2026-03-11 21:23:29 +0530

    Create increase_multiple_of_4_streak.java

---
+++ increase_multiple_of_4_streak.java
@@ -0,0 +1,14 @@
+public class IncreasingMultipleOfFourStreak {
+    public static void main(String[] args) {
+        int[] arr = {4, 8, 12, 3, 16, 20};
+        int count = 0, max = 0;
+
+        for (int n : arr) {
+            if (n % 4 == 0) {
+                count++;
+                max = Math.max(max, count);
+            } else count = 0;
+        }
+        System.out.println(max);
+    }
+}

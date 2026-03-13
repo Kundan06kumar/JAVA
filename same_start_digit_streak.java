16726961c568b9d456d03968c3b7f601dce8eeaf
Kundan06kumar <kundan0618.in@gmail.com>
2026-03-13 22:30:49 +0530

    Create same_start_digit_streak.java

---
+++ same_start_digit_streak.java
@@ -0,0 +1,23 @@
+public class StartingDigitStreak {
+
+    static int firstDigit(int n) {
+        while (n >= 10) n /= 10;
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {123, 145, 167, 245, 278};
+        int count = 1, max = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (firstDigit(arr[i]) == firstDigit(arr[i - 1]))
+                count++;
+            else
+                count = 1;
+
+            max = Math.max(max, count);
+        }
+
+        System.out.println(max);
+    }
+}

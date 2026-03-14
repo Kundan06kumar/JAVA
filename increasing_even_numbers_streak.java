79da761b3be01df28ace7abf29d09b31107d21eb
Kundan06kumar <kundan0618.in@gmail.com>
2026-03-14 23:23:07 +0530

    Create increasing_even_numbers_streak.java

---
+++ increasing_even_numbers_streak.java
@@ -0,0 +1,16 @@
+public class IncreasingEvenStreak {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 3, 8, 10};
+        int count = 0, max = 0;
+
+        for (int n : arr) {
+            if (n % 2 == 0) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                count = 0;
+            }
+        }
+        System.out.println(max);
+    }
+}

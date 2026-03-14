806c656d80908d4d90fec66ab59b52ace9e0b163
Kundan06kumar <kundan0618.in@gmail.com>
2026-03-14 23:22:20 +0530

    Create same_number_of_letter_streak.java

---
+++ same_number_of_letter_streak.java
@@ -0,0 +1,16 @@
+public class SameLetterCountStreak {
+    public static void main(String[] args) {
+        String[] words = {"dog", "cat", "sun", "tree", "moon"};
+        int count = 1, max = 1;
+
+        for (int i = 1; i < words.length; i++) {
+            if (words[i].length() == words[i - 1].length())
+                count++;
+            else
+                count = 1;
+
+            max = Math.max(max, count);
+        }
+        System.out.println(max);
+    }
+}

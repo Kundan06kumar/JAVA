2caa0f1cb100c7bfd90e5655333bce6a8462dfd1
Kundan06kumar <kundan0618.in@gmail.com>
2026-03-17 22:56:44 +0530

    Create consecutive_multiples_of_2_streak.java

---
+++ consecutive_multiples_of_2_streak.java
@@ -0,0 +1,14 @@
+public class MultipleOfTwoStreak {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 3, 10, 12};
+        int count = 0, max = 0;
+
+        for (int n : arr) {
+            if (n % 2 == 0) {
+                count++;
+                max = Math.max(max, count);
+            } else count = 0;
+        }
+        System.out.println(max);
+    }
+}

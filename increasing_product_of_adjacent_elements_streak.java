fd337c64107d6f2e87d67809bf150806ec4ba3aa
Kundan06kumar <kundan0618.in@gmail.com>
2026-03-22 21:59:25 +0530

    Create increasing_product_of_adjacent_elements_streak.java

---
+++ increasing_product_of_adjacent_elements_streak.java
@@ -0,0 +1,18 @@
+public class AdjacentProductStreak {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 2, 5, 6};
+        int count = 2, max = 2;
+
+        for (int i = 2; i < arr.length; i++) {
+            int prev = arr[i - 1] * arr[i - 2];
+            int curr = arr[i] * arr[i - 1];
+
+            if (curr > prev)
+                count++;
+            else count = 2;
+
+            max = Math.max(max, count);
+        }
+        System.out.println(max);
+    }
+}

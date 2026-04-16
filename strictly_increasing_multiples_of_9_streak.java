ff97fc558a0223d943c5db00dba2e58404e66cba
Kundan06kumar <kundan0618.in@gmail.com>
2026-04-16 23:47:17 +0530

    Create strictly_increasing_multiples_of_9_streak.java

---
+++ strictly_increasing_multiples_of_9_streak.java
@@ -0,0 +1,17 @@
+public class IncreasingMultipleOfNineStreak {
+    public static void main(String[] args) {
+        int[] arr = {9, 18, 27, 5, 36, 45};
+        int count = 0, max = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 9 == 0 &&
+               (i == 0 || arr[i] > arr[i - 1])) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                count = (arr[i] % 9 == 0) ? 1 : 0;
+            }
+        }
+        System.out.println(max);
+    }
+}

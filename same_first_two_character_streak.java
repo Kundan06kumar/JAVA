dcf9463b9451616a4334c2ae38b4573ad883a230
Kundan06kumar <kundan0618.in@gmail.com>
2026-04-18 23:54:43 +0530

    Create same_first_two_character_streak.java

---
+++ same_first_two_character_streak.java
@@ -0,0 +1,16 @@
+public class FirstTwoCharStreak {
+    public static void main(String[] args) {
+        String[] arr = {"apple", "april", "apex", "bat", "ball"};
+        int count = 1, max = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i].substring(0, 2)
+                .equals(arr[i - 1].substring(0, 2)))
+                count++;
+            else count = 1;
+
+            max = Math.max(max, count);
+        }
+        System.out.println(max);
+    }
+}

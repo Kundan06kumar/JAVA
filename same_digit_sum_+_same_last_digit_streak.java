259abc8efbc559e73b428a8196d65040be3340b1
Kundan06kumar <kundan0618.in@gmail.com>
2026-05-06 23:35:16 +0530

    Create same_digit_sum_+_same_last_digit_streak.java

---
+++ same_digit_sum_+_same_last_digit_streak.java
@@ -0,0 +1,26 @@
+public class DigitSumAndLastDigitStreak {
+
+    static int digitSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 21, 30, 41, 52};
+        int count = 1, max = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (digitSum(arr[i]) == digitSum(arr[i - 1]) &&
+                arr[i] % 10 == arr[i - 1] % 10)
+                count++;
+            else count = 1;
+
+            max = Math.max(max, count);
+        }
+        System.out.println(max);
+    }
+}

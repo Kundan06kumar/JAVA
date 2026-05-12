db924040ccd548ea9858ee8c65b2d40464be48ad
Kundan06kumar <kundan0618.in@gmail.com>
2026-05-12 23:00:09 +0530

    Create increasing_product_of_three_consecutive_element.java

---
+++ increasing_product_of_three_consecutive_element.java
@@ -0,0 +1,21 @@
+public class ThreeProductStreak {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 1, 6};
+
+        int count = 3, max = 3;
+
+        for (int i = 3; i < arr.length; i++) {
+            int prev = arr[i-1] * arr[i-2] * arr[i-3];
+            int curr = arr[i] * arr[i-1] * arr[i-2];
+
+            if (curr > prev) {
+                count++;
+            } else {
+                count = 3;
+            }
+            max = Math.max(max, count);
+        }
+
+        System.out.println(max);
+    }
+}

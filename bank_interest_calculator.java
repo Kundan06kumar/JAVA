1046f5d3cc01b6f258aa923ff25a3c9b06cd5ce1
Kundan06kumar <kundan0618.in@gmail.com>
2026-06-11 22:47:27 +0530

    Create bank_interest_calculator.java

---
+++ bank_interest_calculator.java
@@ -0,0 +1,9 @@
+public class InterestCalculator {
+    public static void main(String[] args) {
+        double principal = 10000;
+        double rate = 7;
+        double interest = (principal * rate) / 100;
+
+        System.out.println("Interest: ₹" + interest);
+    }
+}

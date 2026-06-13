8543b0eff9f2b5c50805528ecd8d15c6710504a7
Kundan06kumar <kundan0618.in@gmail.com>
2026-06-13 22:06:37 +0530

    Create attendance_percentage.java

---
+++ attendance_percentage.java
@@ -0,0 +1,10 @@
+public class Attendance {
+    public static void main(String[] args) {
+        int attended = 85;
+        int total = 100;
+
+        double percentage = (attended * 100.0) / total;
+
+        System.out.println("Attendance: " + percentage + "%");
+    }
+}

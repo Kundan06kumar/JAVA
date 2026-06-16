ab6d76ae3645f40770f9f514487293bbd3f98c38
Kundan06kumar <kundan0618.in@gmail.com>
2026-06-16 23:13:56 +0530

    Create employee_salary_bonus.java

---
+++ employee_salary_bonus.java
@@ -0,0 +1,8 @@
+public class EmployeeBonus {
+    public static void main(String[] args) {
+        double salary = 40000;
+        double bonus = salary * 0.10;
+
+        System.out.println("Bonus: ₹" + bonus);
+    }
+}

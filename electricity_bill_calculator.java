10824b6079cff6f163fca5570eb1c727c29d507d
Kundan06kumar <kundan0618.in@gmail.com>
2026-06-19 23:00:36 +0530

    Add electricity bill calculator implementation

---
+++ electricity_bill_calculator.java
@@ -0,0 +1,8 @@
+public class ElectricityBill {
+    public static void main(String[] args) {
+        int units = 250;
+        double bill = units * 6.5;
+
+        System.out.println("Total Bill: ₹" + bill);
+    }
+}

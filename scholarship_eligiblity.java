0154519545dcb5ef0df23d8d509b91ea090ee24f
Kundan06kumar <kundan0618.in@gmail.com>
2026-06-23 23:01:08 +0530

    Create scholarship_eligiblity.java

---
+++ scholarship_eligiblity.java
@@ -0,0 +1,10 @@
+ public class Scholarship {
+    public static void main(String[] args) {
+        double percentage = 85;
+
+        if (percentage >= 80)
+            System.out.println("Eligible for Scholarship");
+        else
+            System.out.println("Not Eligible");
+    }
+}

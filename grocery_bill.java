39a51bac5652997eab57ee87c10b0a145cd3a18a
Kundan06kumar <kundan0618.in@gmail.com>
2026-06-24 23:03:14 +0530

    Create grocery_bill.java

---
+++ grocery_bill.java
@@ -0,0 +1,11 @@
+public class GroceryBill {
+    public static void main(String[] args) {
+        double rice = 450;
+        double sugar = 120;
+        double milk = 60;
+
+        double total = rice + sugar + milk;
+
+        System.out.println("Total Bill: ₹" + total);
+    }
+}

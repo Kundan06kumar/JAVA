dae894786f9ba9d08dcfd8ffbf9ff4e68e6ce81c
Kundan06kumar <kundan0618.in@gmail.com>
2026-06-25 22:42:02 +0530

    Create traffic_fine_calculator.java

---
+++ traffic_fine_calculator.java
@@ -0,0 +1,10 @@
+public class TrafficFine {
+    public static void main(String[] args) {
+        int speed = 90;
+
+        if (speed > 80)
+            System.out.println("Fine: ₹1000");
+        else
+            System.out.println("No Fine");
+    }
+}

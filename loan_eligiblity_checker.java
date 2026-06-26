6f5236e3ac3a69949824a1f423f0622752ebe241
Kundan06kumar <kundan0618.in@gmail.com>
2026-06-26 23:01:37 +0530

    Create loan_eligiblity_checker.java

---
+++ loan_eligiblity_checker.java
@@ -0,0 +1,10 @@
+public class LoanEligibility {
+    public static void main(String[] args) {
+        int salary = 35000;
+
+        if (salary >= 30000)
+            System.out.println("Loan Approved");
+        else
+            System.out.println("Loan Rejected");
+    }
+}

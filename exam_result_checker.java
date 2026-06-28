a2109ed1556f89beba7f6cb74dc7ff53ca83a74e
Kundan06kumar <kundan0618.in@gmail.com>
2026-06-28 23:25:45 +0530

    Create exam_result_checker.java

---
+++ exam_result_checker.java
@@ -0,0 +1,10 @@
+public class ExamResult {
+    public static void main(String[] args) {
+        int marks = 38;
+
+        if (marks >= 33)
+            System.out.println("Pass");
+        else
+            System.out.println("Fail");
+    }
+}

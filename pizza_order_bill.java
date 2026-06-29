f3b20519052f9d027382a09634a5ac4513bde60e
Kundan06kumar <kundan0618.in@gmail.com>
2026-06-29 22:40:17 +0530

    Create pizza_order_bill.java

---
+++ pizza_order_bill.java
@@ -0,0 +1,10 @@
+public class PizzaOrder {
+    public static void main(String[] args) {
+        int pizzas = 3;
+        int pricePerPizza = 299;
+
+        int total = pizzas * pricePerPizza;
+
+        System.out.println("Pizza Bill: ₹" + total);
+    }
+}

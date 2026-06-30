d6d35249bd4bc1229f61b0f4888dbd568c7cc6b3
Kundan06kumar <kundan0618.in@gmail.com>
2026-06-30 23:07:00 +0530

    Create gym_membership_fee.java

---
+++ gym_membership_fee.java
@@ -0,0 +1,10 @@
+public class GymMembership {
+    public static void main(String[] args) {
+        int months = 3;
+        int feePerMonth = 1200;
+
+        int totalFee = months * feePerMonth;
+
+        System.out.println("Total Fee: ₹" + totalFee);
+    }
+}

5901738e1906a823ffbff6062594e1ed44c8a384
Kundan06kumar <kundan0618.in@gmail.com>
2026-07-02 22:49:38 +0530

    Create library_fine_calculator.java

---
+++ library_fine_calculator.java
@@ -0,0 +1,10 @@
+public class LibraryFine {
+    public static void main(String[] args) {
+        int lateDays = 7;
+        int finePerDay = 10;
+
+        int totalFine = lateDays * finePerDay;
+
+        System.out.println("Library Fine: ₹" + totalFine);
+    }
+}

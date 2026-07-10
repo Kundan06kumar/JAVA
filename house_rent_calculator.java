0769ad7e6893c4d895bb4c0c1b4b288bf3f8b59a
Kundan06kumar <kundan0618.in@gmail.com>
2026-07-10 22:59:34 +0530

    Create house_rent_calculator.java

---
+++ house_rent_calculator.java
@@ -0,0 +1,10 @@
+public class HouseRent {
+    public static void main(String[] args) {
+        int monthlyRent = 12000;
+        int months = 6;
+
+        int totalRent = monthlyRent * months;
+
+        System.out.println("Total Rent: ₹" + totalRent);
+    }
+}

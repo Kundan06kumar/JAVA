6215b671cd47862c9af88f5875e112fa4c53c1f3
Kundan06kumar <kundan0618.in@gmail.com>
2026-07-11 22:06:44 +0530

    Create cab_fare_calculator.java

---
+++ cab_fare_calculator.java
@@ -0,0 +1,10 @@
+public class CabFare {
+    public static void main(String[] args) {
+        double distance = 18;
+        double farePerKm = 15;
+
+        double fare = distance * farePerKm;
+
+        System.out.println("Cab Fare: ₹" + fare);
+    }
+}

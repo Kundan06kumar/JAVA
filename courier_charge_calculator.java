b7158c39d230cb5fdc93a41a647d6b0755142e64
Kundan06kumar <kundan0618.in@gmail.com>
2026-07-12 13:12:33 +0530

    Create courier_charge_calculator.java

---
+++ courier_charge_calculator.java
@@ -0,0 +1,10 @@
+public class CourierCharge {
+    public static void main(String[] args) {
+        double weight = 2.5;
+        double chargePerKg = 80;
+
+        double total = weight * chargePerKg;
+
+        System.out.println("Courier Charge: ₹" + total);
+    }
+}

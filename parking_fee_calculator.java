bfb5afe39ce90f3c416f446fa4159bfb64cf80d1
Kundan06kumar <kundan0618.in@gmail.com>
2026-07-14 22:21:23 +0530

    Create parking_fee_calculator.java

---
+++ parking_fee_calculator.java
@@ -0,0 +1,10 @@
+public class ParkingFee {
+    public static void main(String[] args) {
+        int hours = 5;
+        int chargePerHour = 30;
+
+        int total = hours * chargePerHour;
+
+        System.out.println("Parking Fee: ₹" + total);
+    }
+}

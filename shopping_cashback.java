9efbce4200725bc517018fbd45b62480638a207e
Kundan06kumar <kundan0618.in@gmail.com>
2026-07-18 22:41:44 +0530

    Create shopping_cashback.java

---
+++ shopping_cashback.java
@@ -0,0 +1,8 @@
+public class ShoppingCashback {
+    public static void main(String[] args) {
+        double purchase = 4500;
+        double cashback = purchase * 0.05;
+
+        System.out.println("Cashback: ₹" + cashback);
+    }
+}

0130c13be6a7c4fbf9fc488012a26f47d79ca603
Kundan06kumar <kundan0618.in@gmail.com>
2026-07-19 22:57:17 +0530

    Create parcel_delivery_status.java

---
+++ parcel_delivery_status.java
@@ -0,0 +1,10 @@
+public class ParcelStatus {
+    public static void main(String[] args) {
+        boolean delivered = false;
+
+        if (delivered)
+            System.out.println("Parcel Delivered");
+        else
+            System.out.println("Out for Delivery");
+    }
+}

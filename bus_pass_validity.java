c857fa2ce9ff7d790d3507a31699345c3256d988
Kundan06kumar <kundan0618.in@gmail.com>
2026-07-20 22:46:32 +0530

    Create bus_pass_validity.java

---
+++ bus_pass_validity.java
@@ -0,0 +1,10 @@
+public class BusPass {
+    public static void main(String[] args) {
+        int daysLeft = 12;
+
+        if (daysLeft > 0)
+            System.out.println("Pass Valid");
+        else
+            System.out.println("Pass Expired");
+    }
+}

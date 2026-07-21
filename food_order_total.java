f83cd7fb2421254f2dc7150d61f878c3301ed094
Kundan06kumar <kundan0618.in@gmail.com>
2026-07-21 22:10:41 +0530

    Add FoodOrder class to calculate total order cost

---
+++ food_order_total.java
@@ -0,0 +1,11 @@
+public class FoodOrder {
+    public static void main(String[] args) {
+        double burger = 150;
+        double fries = 90;
+        double drink = 60;
+
+        double total = burger + fries + drink;
+
+        System.out.println("Order Total: ₹" + total);
+    }
+}

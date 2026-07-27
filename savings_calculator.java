879d4c3c40817ce0c9e4fc83ec916960eac93056
Kundan06kumar <kundan0618.in@gmail.com>
2026-07-27 23:08:51 +0530

    Create savings_calculator.java

---
+++ savings_calculator.java
@@ -0,0 +1,10 @@
+public class SavingsCalculator {
+    public static void main(String[] args) {
+        double income = 50000;
+        double expenses = 35000;
+
+        double savings = income - expenses;
+
+        System.out.println("Monthly Savings: ₹" + savings);
+    }
+}

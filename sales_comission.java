7d097c4d44300d398ff641a9834ad461b9a04046
Kundan06kumar <kundan0618.in@gmail.com>
2026-07-28 23:45:55 +0530

    Create sales_comission.java

---
+++ sales_comission.java
@@ -0,0 +1,8 @@
+public class SalesCommission {
+    public static void main(String[] args) {
+        double sales = 150000;
+        double commission = sales * 0.08;
+
+        System.out.println("Commission: ₹" + commission);
+    }
+}

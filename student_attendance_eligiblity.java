a8b4ad2b079a3b89ccfbf3951266c48e3546b767
Kundan06kumar <kundan0618.in@gmail.com>
2026-07-29 21:27:42 +0530

    Add AttendanceEligibility class for exam eligibility check

---
+++ student_attendance_eligiblity.java
@@ -0,0 +1,10 @@
+public class AttendanceEligibility {
+    public static void main(String[] args) {
+        double attendance = 78;
+
+        if (attendance >= 75)
+            System.out.println("Eligible for Exam");
+        else
+            System.out.println("Not Eligible");
+    }
+}

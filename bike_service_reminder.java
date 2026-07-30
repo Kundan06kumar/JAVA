1547673041c86133a543d8f478e83396e2239131
Kundan06kumar <kundan0618.in@gmail.com>
2026-07-30 23:56:30 +0530

    Create bike_service_reminder.java

---
+++ bike_service_reminder.java
@@ -0,0 +1,10 @@
+public class BikeServiceReminder {
+    public static void main(String[] args) {
+        int kilometres = 3200;
+
+        if (kilometres >= 3000)
+            System.out.println("Service Due");
+        else
+            System.out.println("No Service Required");
+    }
+}

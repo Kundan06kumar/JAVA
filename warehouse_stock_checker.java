050dacbf53bdc33c361dcfe39cbb0ef848455fd0
Kundan06kumar <kundan0618.in@gmail.com>
2026-08-01 21:50:31 +0530

    Create warehouse_stock_checker.java

---
+++ warehouse_stock_checker.java
@@ -0,0 +1,10 @@
+public class WarehouseStock {
+    public static void main(String[] args) {
+        int stock = 15;
+
+        if (stock < 20)
+            System.out.println("Restock Required");
+        else
+            System.out.println("Stock Available");
+    }
+}

134112590d0c76815aed70844ea8890865c55ab0
Kundan06kumar <kundan0618.in@gmail.com>
2026-08-02 23:52:24 +0530

    Create car_mileage_calculator.java

---
+++ car_mileage_calculator.java
@@ -0,0 +1,10 @@
+public class CarMileage {
+    public static void main(String[] args) {
+        double distance = 450;
+        double fuel = 30;
+
+        double mileage = distance / fuel;
+
+        System.out.println("Mileage: " + mileage + " km/l");
+    }
+}

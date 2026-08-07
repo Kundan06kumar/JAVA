64ac5994c6902d11d695862df30bb9f7d22c1afd
Kundan06kumar <kundan0618.in@gmail.com>
2026-08-07 22:11:40 +0530

    Create semester_fee_calculator.java

---
+++ semester_fee_calculator.java
@@ -0,0 +1,11 @@
+public class SemesterFee {
+    public static void main(String[] args) {
+        double tuitionFee = 58000;
+        double examFee = 2500;
+        double libraryFee = 1500;
+
+        double total = tuitionFee + examFee + libraryFee;
+
+        System.out.println("Semester Fee: ₹" + total);
+    }
+}

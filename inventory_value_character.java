1360bb9627fb727270d2a7c86955cb56f80d5e74
Kundan06kumar <kundan0618.in@gmail.com>
2026-08-10 22:35:33 +0530

    Create inventory_value_character.java

---
+++ inventory_value_character.java
@@ -0,0 +1,10 @@
+public class InventoryValue {
+    public static void main(String[] args) {
+        int quantity = 120;
+        double price = 85.5;
+
+        double value = quantity * price;
+
+        System.out.println("Inventory Value: ₹" + value);
+    }
+}

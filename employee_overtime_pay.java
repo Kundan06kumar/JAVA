4bcda8b1f076f0c736a18ab8790cae48584c1892
Kundan06kumar <kundan0618.in@gmail.com>
2026-08-11 23:54:01 +0530

    Create employee_overtime_pay.java

---
+++ employee_overtime_pay.java
@@ -0,0 +1,10 @@
+public class OvertimePay {
+    public static void main(String[] args) {
+        int overtimeHours = 12;
+        int ratePerHour = 300;
+
+        int overtimePay = overtimeHours * ratePerHour;
+
+        System.out.println("Overtime Pay: ₹" + overtimePay);
+    }
+}

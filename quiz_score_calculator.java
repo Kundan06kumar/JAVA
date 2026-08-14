fa609dd562ec57670a1259e7fabb421af2e571ca
Kundan06kumar <kundan0618.in@gmail.com>
2026-08-14 23:31:07 +0530

    Create quiz_score_calculator.java

---
+++ quiz_score_calculator.java
@@ -0,0 +1,10 @@
+public class QuizScore {
+    public static void main(String[] args) {
+        int correct = 18;
+        int wrong = 2;
+
+        int score = correct * 4 - wrong;
+
+        System.out.println("Final Score: " + score);
+    }
+}

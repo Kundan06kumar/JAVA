89466a53a3be20b8487a7ce3866d25094d7e7ead
Kundan06kumar <kundan0618.in@gmail.com>
2026-09-22 23:15:22 +0530

    Create employee_leave_balance.java

---
+++ employee_leave_balance.java
@@ -0,0 +1,12 @@
+public class LeaveBalance {
+    public static void main(String[] args) {
+        int totalLeaves = 20;
+        int usedLeaves = 7;
+
+        int remaining = totalLeaves - usedLeaves;
+
+        System.out.println("Total Leaves: " + totalLeaves);
+        System.out.println("Used Leaves: " + usedLeaves);
+        System.out.println("Remaining Leaves: " + remaining);
+    }
+}
